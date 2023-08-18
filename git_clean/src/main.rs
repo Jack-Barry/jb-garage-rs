4e5bb502b148f8aa26dc1755f5f688a9dad7f64b
Jack Barry <Jack@Barry.onl>
2023-08-18 07:47:55 -0600

    wip: Add checks to skip current and default branches

--- git_clean/src/main.rs
+++ git_clean/src/main.rs
@@ -1,5 +1,5 @@
 use dialoguer::{theme::ColorfulTheme, Confirm};
-use git2::{Branch, BranchType, Error, Repository};
+use git2::{Branch, BranchType, Direction, Error, Repository};
 use std::env;
 
 fn main() {
@@ -13,12 +13,14 @@ fn main() {
         Err(e) => panic!("Failed to open repo: {}", e),
     };
 
+    let default_branch_name = get_default_branch(&repo);
+
     let branches = match repo.branches(Some(BranchType::Local)) {
         Ok(branches) => branches,
         Err(e) => panic!("Failed to get branches: {}", e),
     };
 
-    branches.for_each(|branch| handle_branch(&repo, branch));
+    branches.for_each(|branch| handle_branch(&repo, &default_branch_name, branch));
 }
 
 fn prompt_user_for_delete(name: &str) -> bool {
@@ -28,20 +30,76 @@ fn prompt_user_for_delete(name: &str) -> bool {
         .unwrap()
 }
 
-fn handle_branch(repo: &Repository, branch: Result<(Branch, BranchType), Error>) {
-    match branch {
-        Ok(mut verified_branch) => match verified_branch.0.name() {
-            Ok(branch_name) => match branch_name {
-                Some(branch_name_str) => {
-                    let branch_name_str_copy = branch_name_str.to_string();
-                    delete_local_branch(repo, &mut verified_branch, &branch_name_str_copy)
+fn get_default_branch(repo: &Repository) -> Option<String> {
+    match repo.find_remote("origin") {
+        Ok(mut remote) => match remote.connect(Direction::Fetch) {
+            Ok(_) => {
+                let default_branch_result = remote.default_branch();
+                match remote.disconnect() {
+                    Err(e) => {
+                        println!("Unable to disconnect from remote: {}", e)
+                    }
+                    _ => {}
+                };
+
+                match default_branch_result {
+                    Ok(default_branch) => match String::from_utf8(default_branch.to_vec()) {
+                        Ok(default_branch_str) => Some(default_branch_str),
+                        Err(e) => {
+                            println!("Unable to get string from default branch: {}", e);
+                            None
+                        }
+                    },
+                    _ => None,
                 }
-                None => {}
-            },
+            }
             Err(e) => {
-                println!("Unable to get name of branch: {}", e)
+                println!("Error connecting to remote: {}", e);
+                None
             }
         },
+        Err(e) => {
+            println!("Error finding remote: {}", e);
+            None
+        }
+    }
+}
+
+fn handle_branch(
+    repo: &Repository,
+    default_branch: &Option<String>,
+    branch: Result<(Branch, BranchType), Error>,
+) {
+    match branch {
+        Ok(mut verified_branch) => {
+            // skip the current branch
+            if verified_branch.0.is_head() {
+                return;
+            }
+
+            match verified_branch.0.name() {
+                Ok(branch_name) => match branch_name {
+                    Some(branch_name_str) => {
+                        match default_branch {
+                            Some(default_branch_str) => {
+                                // Skip deleting the default branch
+                                if default_branch_str.replace("refs/heads/", "") == branch_name_str
+                                {
+                                    return;
+                                }
+                            }
+                            _ => (),
+                        }
+                        let branch_name_str_copy = branch_name_str.to_string();
+                        delete_local_branch(repo, &mut verified_branch, &branch_name_str_copy)
+                    }
+                    _ => (),
+                },
+                Err(e) => {
+                    println!("Unable to get name of branch: {}", e)
+                }
+            }
+        }
         Err(e) => {
             println!("Unable to use branch: {}", e)
         }
@@ -57,10 +115,10 @@ fn delete_local_branch(
     if will_delete_local_branch {
         handle_upstream_branch(repo, &verified_branch);
         match verified_branch.0.delete() {
-            Ok(_) => {}
             Err(e) => {
                 println!("Error when deleting local branch: {}", e)
             }
+            _ => (),
         };
     }
 }
@@ -70,11 +128,11 @@ fn handle_upstream_branch(repo: &Repository, branch: &(Branch, BranchType)) {
         Some(branch_ref) => match repo.branch_upstream_remote(branch_ref) {
             Ok(remote) => match remote.as_str() {
                 Some(remote_str) => delete_upstream_branch(repo, branch_ref, remote_str),
-                None => {}
+                _ => (),
             },
-            Err(_) => {}
+            _ => (),
         },
-        None => {}
+        _ => (),
     }
 }
 
@@ -87,10 +145,10 @@ fn delete_upstream_branch(repo: &Repository, branch_ref: &str, remote_str: &str)
 
         match repo.find_remote(remote_str) {
             Ok(mut repo_remote) => match repo_remote.push(refspecs, None) {
-                Ok(_) => {}
                 Err(e) => {
                     println!("Encountered trouble deleting remote branch: {}", e)
                 }
+                _ => (),
             },
             Err(e) => {
                 println!("Failed to find remote: {}", e)
